b0c0250cf510900fface195de744a616a02a1394
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2021-03-15 23:28:12 -0700

    Agregar vista vacia

---
+++ laravel_app/resources/views/team.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipo GameFlake</title>
+</head>
+<body>
+    <h1>Equipo de GameFlake&#128126;</h1>
+
+    <div>
+        <p>Foto o imagen representativa.</p>
+        <p>Nombre.</p>
+        <p>Matrícula.</p>
+        <p>Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</p>
+    </div>
+
+    <div>
+        <p>Foto o imagen representativa.</p>
+        <p>Nombre.</p>
+        <p>Matrícula.</p>
+        <p>Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</p>
+    </div>
+
+    <div>
+        <p>Foto o imagen representativa.</p>
+        <p>Nombre.</p>
+        <p>Matrícula.</p>
+        <p>Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</p>
+    </div>
+    
+</body>
+</html>
