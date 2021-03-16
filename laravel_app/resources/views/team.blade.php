6c36488f3f144ab01510c2ee78a96342a00fff3f
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2021-03-16 00:44:37 -0700

    Agregar tarjetas

--- laravel_app/resources/views/team.blade.php
+++ laravel_app/resources/views/team.blade.php
@@ -3,46 +3,76 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Equipo GameFlake</title>
 
     <!-- Boostrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- AOS CSS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
 </head>
 <body>
-    <div class="container">
-        <h1>Equipo de GameFlake&#128126;</h1>
+    <div class="container d-flex flex-column align-items-center ">
+        <h1 class="text-center my-4">&#128126; Equipo de GameFlake &#128126;</h1>
 
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
         
             <div class="col">
-                <img src="" alt="">
-                <p>Nombre.</p>
-                <p>Matrícula.</p>
-                <p>Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</p>
+                <div class="card shadow" data-aos="zoom-out" data-aos-duration="1250">
+                    <img class="card-img-top" src="{{url('img/adolfo.jpg')}}" alt="Card image cap">
+                    <img src="" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Adolfo Acosta &#127844;</h5>
+                        <p class="card-text">ID-A01705249</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
             <div class="col">
-                <p>Foto o imagen representativa.</p>
-                <p>Nombre.</p>
-                <p>Matrícula.</p>
-                <p>Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</p>
+                <div class="card shadow" data-aos="zoom-out" data-aos-duration="1250">
+                    <img class="card-img-top" src="{{url('img/guillermo.jpg')}}" alt="Card image cap">
+                    <img src="" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Guillermo Espino &#129472;</h5>
+                        <p class="card-text">ID-A01704354</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
             <div class="col">
-                <p>Foto o imagen representativa.</p>
-                <p>Nombre.</p>
-                <p>Matrícula.</p>
-                <p>Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</p>
+                <div class="card shadow" data-aos="zoom-out" data-aos-duration="1250">
+                    <img class="card-img-top" src="{{url('img/valeria.jpg')}}" alt="Card image cap">
+                    <img src="" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Valeria Guerra &#129360;</h5>
+                        <p class="card-text">ID-A01705318</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Descripción de su experiencia en desarrollo web (lenguajes, frameworks, librerías, etc.).</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+    <div class="min-vh-100" style="background: grey">
     </div>
     
 
     <!-- Boostrap JS: jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <!-- AOS JS -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>AOS.init();</script>
 </body>
 </html>
