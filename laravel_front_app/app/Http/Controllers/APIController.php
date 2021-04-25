63ea5acfa574902cadb31bd093a9d3695ce1ae62
valeriagdlo99 <58562805+valeriagdlo@users.noreply.github.com>
2021-04-25 18:31:13 -0500

    creacion proyecto API

---
+++ laravel_front_app/app/Http/Controllers/APIController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Ofertas; 
+/*\Ofertas;*/
+use Illuminate\Http\Request;
+
+class APIController extends Controller
+{
+    public function create(){
+    }
+}
