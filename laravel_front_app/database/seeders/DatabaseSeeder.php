527ea60a54d10c834b79d6d9d6fdb6648108521c
GlinkIsCoding <carlos.espino.m@gmail.com>
2021-04-28 00:44:57 -0500

    Añadir Seeder de Consolas

--- laravel_front_app/database/seeders/DatabaseSeeder.php
+++ laravel_front_app/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TituloSeeder::class,
             CondicionSeeder::class,
+            ConsolaSeeder::class,
         ]);
     }
 }
