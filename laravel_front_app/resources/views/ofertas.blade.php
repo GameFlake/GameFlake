8c698103f52ec8612af85e41ce4f1abc45024266
valeriagdlo99 <58562805+valeriagdlo@users.noreply.github.com>
2021-05-01 00:48:56 -0500

    ofertas front terminado

--- laravel_front_app/resources/views/ofertas.blade.php
+++ laravel_front_app/resources/views/ofertas.blade.php
@@ -2,41 +2,43 @@
 
 @section('pageTitle', "GameFlake")
 
-@section('header')
 
-<h1 class='text-center titulo white-text'><i class="material-icons large white-text">arrow_back</i> Ofertas recibidas</h1>
-<br>
-<br>
+
+@section('header')
+  <div class="container">
+    <h1 class='text-center titulo white-text'><i class="material-icons large white-text">arrow_back</i> Ofertas recibidas</h1>
+    <br>
+    <br>
+  </div>
 @endsection
 
 @section('mainContent')
-
 <div class="container">
 
-<table  class="responsive-table striped bordered" >
+      <table  class="responsive-table" >
 
-
-
-@php
-var_dump($ofertaquery);
-@endphp
-        <thead>
-          <tr>
-          <dl>
-          
-</dl>
-              <th>Nombre</th>
-              <th>Queire cambiar</th>
-              <th>Por tu</th>
+        <thead class="grey darken-4 ">
+          <tr class="white-text  ">
+              <th class="center-align">Nombre</th>
+              <th class="center-align">Quiere cambiar</th>
+              <th class="center-align">Por tu</th>
+              <th class="center-align">Aceptar / Rechazar </th>
+              <th class="center-align">Estado</th>
           </tr>
         </thead>
 
-        <tbody>
+        <tbody class="indigo darken-4 white-text">
         @foreach ($ofertaquery as $oferta)
             <tr>
-              <td>{{ $oferta["UOFertante"] }}</td>
-              <td>{{ $oferta["TO"] }}</td>
-              <td>{{ $oferta["TR"] }}</td>
+              <td class="center-align">{{ $oferta["UOFertante"] }}</td>
+              <td class="center-align">{{ $oferta["TO"] }}</td>
+              <td class="center-align">{{ $oferta["TR"] }}</td>
+              <td class="center-align">
+                <a href="#"><i class="material-icons medium green-text">check</i></a>
+                <a href="#"><i class="material-icons medium red-text">close</i></a>  
+              </td>
+              <td class="center-align"><a class="waves-effect waves-light btn">Pendiente</a></td>
+              
             </tr>
         @endforeach
 
@@ -46,9 +48,46 @@ var_dump($ofertaquery);
       <br>
       <br>
       <br>
+      
+  <hr>
+  <br>
+  <br>
+  <h1 class='text-center titulo white-text'> Ofertas realizadas</h1>
+
+  <br>
+  <br>
+  <table  class="responsive-table" >
+
+        <thead class="grey darken-4 ">
+          <tr class="white-text  ">
+              <th class="center-align">Quiero cambiar</th>
+              <th class="center-align">Por</th>
+              <th class="center-align">De</th>
+              <th class="center-align">Eliminar</th>
+          </tr>
+        </thead>
+
+        <tbody class="indigo darken-4 white-text">
+        @foreach ($ofertaquery as $oferta)
+            <tr>
+              <td class="center-align">{{ $oferta["UOFertante"] }}</td>
+              <td class="center-align">{{ $oferta["TO"] }}</td>
+              <td class="center-align">{{ $oferta["TR"] }}</td>
+              <td class="center-align"><a href="#"><i class="material-icons medium red-text">close</i></a> </td>
+            </tr>
+        @endforeach
+
+          
+        </tbody>
+      </table>
 
 
-
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
 
 </div>
 
