24714c34233f090db12874b1f228f20585bffb38
Adolfo Acosta Castro <adolfoacostacastro@gmail.com>
2021-06-07 14:17:37 -0600

    Corregir acomodo de catalogo y layout

--- laravel_front_app/resources/views/catalogo.blade.php
+++ laravel_front_app/resources/views/catalogo.blade.php
@@ -3,22 +3,17 @@
 @section('pageTitle', "GameFlake")
 
 @section('header')
-
 <div class="container">
-<h1 class='text-center titulo white-text'>Catálogo</h1>
+  <h1 class='text-center titulo white-text'>Catálogo</h1>
 </div>
-<br>
-<br>
-
 @endsection
 
 @section('mainContent')
-
 <div class="container">
-@if (count($titulosquery)!=0)  
+  @if (count($titulosquery)!=0)  
   <div class="row">
     @foreach($titulosquery as $titulo)
-    <div class="col s4">
+    <div class="col s12 m6 l4">
       <div class="card small">
         <div class="card-image waves-effect waves-block waves-light">
           <img class="activator" height="160" src={{ asset($titulo['imgRuta']) }}>
@@ -31,25 +26,16 @@
     </div>
     @endforeach
   </div>
-  @else
-       
-       <div class="col s12 m7">
-         <div class="card horizontal  blue lighten-2">
-           <div class="card-stacked">
-             <div class="card-content center-align">
-               <h3>¡No hay títulos registrados en el catálogo!</h3>
-             </div>
-           </div>
-         </div>
-       </div>
-       <br>
-       <br>
-       <br>
-       <br>
-        
-             
-        @endif 
+  @else 
+  <div class="col s12 m7">
+    <div class="card horizontal  blue lighten-2">
+      <div class="card-stacked">
+        <div class="card-content center-align">
+          <h3>¡No hay títulos registrados en el catálogo!</h3>
+        </div>
+      </div>
+    </div>
+  </div>    
+  @endif 
 </div>
-
-      
 @endsection
--- laravel_front_app/resources/views/layouts/layout.blade.php
+++ laravel_front_app/resources/views/layouts/layout.blade.php
@@ -5,18 +5,20 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>@yield('pageTitle')</title>
 
-    <!--Icons-->
+    <link type="text/css" rel="stylesheet" href="{{asset ('assets/css/materialize.min.css')}}" media="screen,projection">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-     <!--Import materialize.css-->
-     <link type="text/css" rel="stylesheet" href="{{asset ('assets/css/materialize.min.css')}}" media="screen,projection">
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!--Custom CSS-->
-      <link rel="stylesheet" href="{{ url('css/styles.css')}}">
-      <!-----Sweetalert--->
-      <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="icon" href="{{ asset('/img/logo_paredmart.svg') }}">
+
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="{{ url('css/styles.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{ asset('/css/utilities.css') }}" />
+
+    <!--Sweetalert--->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
@@ -27,74 +29,72 @@
 </head>
 
 <body class="blue darken-1">
-<nav>
+  <nav>
     <div class="nav-wrapper indigo darken-4">
-      <img src="{{asset('images/logo.JPG')}}" class= "responsive-img" width="100">
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <a href="#" class="brand-logo flow-text ml-2">
+        GameFlake
+      </a>
+      <a href="#" data-target="sidebar" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-            <li><a href="#">Inicio</a></li>
+            <li><a href="#">        Inicio</a></li>
             <li><a href="/catalogo">Catálogo</a></li>
-            <li><a href="#">Mis juegos</a></li>
-            <li><a href="/ofertas">Mis ofertas</a></li>
-            <li><a href="#">Mis coincidencias</a></li>
-            <li><a href="#">Mi perfil</a></li>
-            <a class= "blue accent-4 btn">Cerrar sesión</a>
+            <li><a href="#">        Mis juegos</a></li>
+            <li><a href="/ofertas"> Mis ofertas</a></li>
+            <a class="blue accent-4 btn">Cerrar sesión</a>
       </ul>
     </div>
   </nav>
-
-  <ul class="sidenav right" id="mobile-demo">
+  <ul class="sidenav right" id="sidebar">
         <li><a href="#">Inicio</a></li>
         <li><a href="#">Catálogo</a></li>
         <li><a href="#">Mis juegos</a></li>
         <li><a href="#">Mis ofertas</a></li>
-        <li><a href="#">Mis coincidencias</a></li>
-        <li><a href="#">Mi perfil</a></li>
-        <a class= "blue accent-4 btn">Cerrar sesión</a>
+        <a class="blue accent-4 btn mt-2 ml-4">Cerrar sesión</a>
   </ul>
 
-<script>
- $(document).ready(function() {
-    $('.sidenav').sidenav();
-});
-</script>       
+  @yield('register')
+  @yield('header')
 
-    @yield('register')
-    @yield('header')
-    <main class="containers-fluid">
-        @yield('mainContent')
+  <main class="containers-fluid">
+      @yield('mainContent')
 
-
-        <footer class="page-footer indigo darken-4 ">
-          <div class="container ">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">¡Conoce más sobre nosotros!</h5>
-                <p class="grey-text text-lighten-4">Aplicación para que intercambies tus juegos.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Información</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Inicio</a></li>
-                  <li><a class="grey-text text-lighten-3" href="/catalogo">Catálogo</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Mis juego</a></li>
-                  <li><a class="grey-text text-lighten-3" href="/ofertas">Mis ofertas</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Mis coincidencias</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Mi perfil</a></li>
-                </ul>
-              </div>
+      <footer class="page-footer indigo darken-4 ">
+        <div class="container ">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">¡Conoce más sobre nosotros!</h5>
+              <p class="grey-text text-lighten-4">Aplicación para que intercambies tus juegos.</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">Información</h5>
+              <ul>
+                <li><a class="grey-text text-lighten-3" href="#!">Inicio</a></li>
+                <li><a class="grey-text text-lighten-3" href="/catalogo">Catálogo</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Mis juego</a></li>
+                <li><a class="grey-text text-lighten-3" href="/ofertas">Mis ofertas</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Mis coincidencias</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Mi perfil</a></li>
+              </ul>
             </div>
           </div>
-          <div class="footer-copyright indigo darken-3">
-            <div class="container">
-            © 2021 Gameflake 
-            <a class="grey-text text-lighten-4 right" href="#!">Aviso de privacidad</a>
-            </div>
+        </div>
+        <div class="footer-copyright indigo darken-3">
+          <div class="container">
+          © 2021 GameFlake 
+          <a class="grey-text text-lighten-4 right" href="#!">Aviso de privacidad</a>
           </div>
-        </footer>
+        </div>
+      </footer>
+    </main>
     
-    <!--JavaScript at end of body for optimized loading-->
     <script type="text/javascript" src="{{asset ('assets/js/materialize.min.js')}}"></script>
-    @stack('scripts')
+    <script>
+    $(document).ready(function() {
+        $('.sidenav').sidenav();
+    });
+    </script>     
 
+    @stack('scripts')
+  
+</body>  
 </html>
